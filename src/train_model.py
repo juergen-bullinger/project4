415dd0b8ea874cfe7770ace45f29ae3aadc280c4
Juergen Bullinger <juergen.bullinger@gmx.net>
2024-04-18 00:26:09 +0200

    updated code in data.py to correctly read the input data and fixed code according to api change in scikit-learn

--- src/train_model.py
+++ src/train_model.py
@@ -3,8 +3,12 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pandas as pd
+from ml.data import process_data
 
 # Add code to load in the data.
+data = pd.read_csv("data/census.csv", sep=',\s*', encoding="utf-8", engine="python")
+
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
