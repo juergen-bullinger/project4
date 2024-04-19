50d6c009272de08f8fa55f4d13f002e6d0a8aa30
Juergen Bullinger <juergen.bullinger@gmx.net>
2024-04-19 16:42:29 +0200

    fixed rest api logic and implemented a basic test

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ import pandas as pd
 import config as cfg
 from ml.data import process_data
 from ml import model
+import utils
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
@@ -23,7 +24,7 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
 ONE_HOT_ENCODER = None
 LABEL_ENCODER = None
 ML_MODEL = None
-CAT_COLUMNS
+CAT_COLUMNS = None
 
 def initialize():
     """
@@ -33,12 +34,18 @@ def initialize():
     global LABEL_ENCODER
     global ML_MODEL
     global CAT_COLUMNS
-    ONE_HOT_ENCODER = cfg.CONFIG["preprocessing"]["one_hot_encoder_file"]
-    LABEL_ENCODER = cfg.CONFIG["preprocessing"]["label_encoder_file"]
-    ML_MODEL = cfg.CONFIG["model"]["file"]
+    ONE_HOT_ENCODER = utils.pickle_load_object(
+        cfg.CONFIG["preprocessing"]["one_hot_encoder_file"]
+    )
+    LABEL_ENCODER = utils.pickle_load_object(
+        cfg.CONFIG["preprocessing"]["label_encoder_file"]
+    )
+    ML_MODEL = utils.pickle_load_object(
+        cfg.CONFIG["model"]["file"]
+    )
     CAT_COLUMNS = cfg.CONFIG["preprocessing"]["categories"]
 
-
+cfg.initialize_global_config()
 initialize()
 
 # Instantiate the app.
@@ -46,24 +53,24 @@ app = FastAPI()
 
 
 class DataRecord(BaseModel):
-    age            : int
-    workclass      : str
-    fnlgt          : int
-    education      : str
-    education_num  : int
-    marital_status : str
-    occupation     : str
-    relationship   : str
-    race           : str
-    sex            : str
-    capital_gain   : int
-    capital_loss   : int
-    hours_per_week : int
-    native_country : str
+    age            : int = Field(title="age", description="age of the person", )
+    workclass      : str = Field(title="work class", description="work class", )
+    fnlgt          : int = Field(title="fnlgt", description="read the docs", )
+    education      : str = Field(title="education", description="type of education", )
+    education_num  : int = Field(title="education num", description="category of education", )
+    marital_status : str = Field(title="marital status", description="married, divorced, single, ...", )
+    occupation     : str = Field(title="occupation", description="occupation", )
+    relationship   : str = Field(title="relationship", description="relationship", )
+    race           : str = Field(title="race", description="race", )
+    sex            : str = Field(title="sex", description="sex", )
+    capital_gain   : int = Field(title="capital gain", description="capital gain")
+    capital_loss   : int = Field(title="capital loss", description="capital loss")
+    hours_per_week : int = Field(title="hours per week", description="number of working hours per week")
+    native_country : str = Field(title="native country", description="native country", )
 
 
 @app.get("/")
-def welcome():
+async def welcome():
     """
     Show a welcome message.
 
@@ -78,14 +85,13 @@ def welcome():
     }
 
 
-@app.post("/inference/")
-def inference(data : List[DataRecord]) -> List[bool]:
+def _inference_list(record_list : List[DataRecord]) -> List[bool]:
     """
     Predict the income category for the given records
 
     Parameters
     ----------
-    data : List[DataRecord]
+    record_list : List[DataRecord]
         The DataRecord represent the data that needs to be predicted.
 
     Returns
@@ -97,15 +103,61 @@ def inference(data : List[DataRecord]) -> List[bool]:
     global LABEL_ENCODER
     global ML_MODEL
     global CAT_COLUMNS
-    records = [record.dict() for record in data]
-    df_data = pd.DataFrame(records)
+    df_data = pd.DataFrame([record.dict() for record in record_list])
     # restore the original column names (- instead of _)
-    df_data.columns = [col.replace("_", "_") for col in df_data.columns]
+    df_data.columns = [col.replace("_", "-") for col in df_data.columns]
+    print(df_data)
     X, *_ = process_data(
         df_data,
         training=False,
+        categorical_features=CAT_COLUMNS, 
+        #label="salary", 
         encoder=ONE_HOT_ENCODER, 
         lb=LABEL_ENCODER
     )
+    print("The shape of X is:", X.shape)
     y_pred = model.inference(ML_MODEL, X)
     return y_pred
+
+
+# example to test this with curl
+# curl -X POST http://127.0.0.1:8000/inference/
+@app.post("/inference_list")
+async def inference_list(body : List[DataRecord]) -> List[bool]:
+    """
+    Predict the income category for the given records
+
+    Parameters
+    ----------
+    record_list : List[DataRecord]
+        The DataRecord represent the data that needs to be predicted.
+
+    Returns
+    -------
+    predictions : List[bool]
+        One prediction per record in data.
+    """
+    return _inference_list(body)
+
+
+# example to test this with curl
+# curl -X POST http://127.0.0.1:8000/inference/
+@app.post("/inference_one")
+async def inference_one(body : DataRecord) -> bool:
+    """
+    Predict the income category for the given records
+
+    Parameters
+    ----------
+    record_list : List[DataRecord]
+        The DataRecord represent the data that needs to be predicted.
+
+    Returns
+    -------
+    prediction : bool
+        Prediction for the record.
+    """
+    return _inference_list([body])
+
+
+print(dir(app))
---
+++ tests/manual_api_test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 15:33:40 2024
+
+@author: juergen
+"""
+
+import json
+import requests as rq
+
+
+
+
+record_1=dict(
+    age=30,
+    workclass="State-gov",
+    fnlgt=77516,
+    education="Bachelors",
+    education_num=13,
+    marital_status="Never-married",
+    occupation="Adm-clerical",
+    relationship="Not-in-family",
+    race="White",
+    sex="Male",
+    capital_gain=2174,
+    capital_loss=0,
+    hours_per_week=40,
+    native_country="United-States"
+)
+
+record_2=dict(
+    age=39,
+    workclass="State-gov",
+    fnlgt=77516,
+    education="Bachelors",
+    education_num=13,
+    marital_status="Never-married",
+    occupation="Adm-clerical",
+    relationship="Not-in-family",
+    race="White",
+    sex="Male",
+    capital_gain=2174,
+    capital_loss=0,
+    hours_per_week=40,
+    native_country="United-States"
+)
+
+record_3=dict(
+    age=52,
+    workclass="Self-emp-not-inc",
+    fnlgt=209642,
+    education="HS-grad",
+    education_num=9,
+    marital_status="Married-civ-spouse",
+    occupation="Exec-managerial",
+    relationship="Husband",
+    race="White",
+    sex="Male",
+    capital_gain=0,
+    capital_loss=0,
+    hours_per_week=45,
+    native_country="United-States"
+)
+
+
+
+
+response = rq.post(
+    "http://127.0.0.1:8000/inference_one?body", 
+#    data={
+#        "record": data,
+#    }
+     #json=json.dumps(data),
+     #data=data,
+     json=record_3,
+)
+
+print(response)
+print(response.json())
+
+
+
+#response = rq.post("http://127.0.0.1:8000/inference", data=dict(data=[data]))
+response = rq.post(
+    "http://127.0.0.1:8000/inference_list", 
+    json=[record_1, record_2, record_3]
+)
+
+print(response)
+print(response.json())
